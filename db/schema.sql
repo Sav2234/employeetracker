ec3880de69bd1416b39f078f8f644b94d5967358
Savion Elsey-Williams <s.e.williams2001@gmail.com>
2021-10-27 17:47:08 -0400

    data1

--- db/schema.sql
+++ db/schema.sql
@@ -2,22 +2,22 @@ DROP DATABASE IF EXISTS mysara_db;
 CREATE DATABASE mysara_db;
 USE mysara_db;
 
-CREATE TABLE department
+CREATE TABLE department;
 (
     id INT PRIMARY KEY AUTO_INCREMENT,
-    name VARCHAR(30) NOT NULL
+    name VARCHAR(30) NOT NULL,
 )
 
-CREATE TABLE role
+CREATE TABLE role;
 (
     id INT PRIMARY KEY AUTO_INCREMENT,
     title VARCHAR(30) NOT NULL,
     salary DECIMAL NOT NULL,
     department_id INT NOT NULL,
-    FOREIGN KEY (department_id) REFERENCES department(id) ON DELETE CASCADE
+    FOREIGN KEY (department_id) REFERENCES department(id) ON DELETE CASCADE,
 )
 
-CREATE TABLE employee
+CREATE TABLE employee;
 (
    id INT PRIMARY KEY AUTO_INCREMENT,
    first_name VARCHAR(30) NOT NULL,
@@ -25,5 +25,5 @@ CREATE TABLE employee
    role_id INT,
    FOREIGN KEY (role_id) REFERENCES role(id) ON DELETE CASCADE,
    manager_id INT,
-   FOREIGN KEY (manager_id) REFERENCES employee(id) ON DELETE SET NULL
+   FOREIGN KEY (manager_id) REFERENCES employee(id) ON DELETE SET NULL,
 )
--- server.js
+++ server.js
@@ -10,7 +10,7 @@ function startMenu() {
             type: "list",
             message: "What would ypu like to do today?",
             name: "action",
-            choices: ["Add Employee", "Add Department", "Add Role", "Nothing", "View all Roles", "View all Employees", "View all Departments"
+            choices: ["Add Employee", "Add Department", "Add Role", "View all Roles", "View all Employees", "View all Departments", "Nothing"
             ]
         }
     ).then(function (response) {
@@ -25,10 +25,6 @@ function startMenu() {
 
         else if (response.action === "Add Role") {
             addRole()
-        }
-
-        else if (response.action === "Nothing") {
-            Creation()
         }
 
         // else if (response.action === "Remove Employee") {
@@ -54,6 +50,9 @@ function startMenu() {
         else if (response.action === "View all Departments") {
             viewDep()
         }
+        else if (response.action === "Nothing") {
+            exitMenu()
+        }
     })
 
     function addEmployee() {
@@ -78,7 +77,7 @@ function startMenu() {
 
             {
                 type: "input",
-                name: "empRoleID",
+                name: "role_id",
                 message: "What is the employee's role ID?",
             },
 
@@ -98,13 +97,13 @@ function startMenu() {
         inquirer.prompt([
             {
                 type: "input",
-                name: "DepName",
+                name: "name",
                 message: "What would you like to call the department?",
             },
 
             {
                 type: "input",
-                name: "DepID",
+                name: "id",
                 message: "What is the department's ID?",
             },
 
@@ -120,25 +119,25 @@ function startMenu() {
         inquirer.prompt([
             {
                 type: "input",
-                name: "role_id",
+                name: "id",
                 message: "Give the role an ID",
             },
 
             {
                 type: "input",
-                name: "role_title",
+                name: "title",
                 message: "What is the role's title?",
             },
 
             {
                 type: "input",
-                name: "role_salary",
+                name: "salary",
                 message: "What is the salary for this role?",
             },
 
             {
                 type: "input",
-                name: "DepID",
+                name: "department_id",
                 message: "What is the corresponding ID of the department for this role?",
             },
         ]).then(response => {
@@ -164,9 +163,14 @@ function addMore() {
         }
 
         else if (response.more === "No") {
-            Creation()
+            exitMenu()
         }
     })
+}
+
+function exitMenu() {
+    console.log("Now ending process")
+    process.exit()
 }
 
 function viewRoles() {
@@ -192,5 +196,6 @@ function viewDep() {
         startMenu()
     })
 }
+
 
 startMenu();
